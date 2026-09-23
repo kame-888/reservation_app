003361448d34318990133c5cb065aff0ebc49225
kame-888 <rioikegame1111@gmail.com>
2026-09-24 08:46:06 +0900

    fix: Userモデルカラム追加, create: deviseメソッドの登録

--- db/migrate/20260923222814_add_more_columns_to_users.rb
+++ db/migrate/20260923222814_add_more_columns_to_users.rb
@@ -1,7 +1,5 @@
-class AddColumnToUser < ActiveRecord::Migration[7.2]
+class AddMoreColumnsToUsers < ActiveRecord::Migration[7.2]
   def change
-    add_column :users, :first_name, :string
-    add_column :users, :family_name, :string
     add_column :users, :confirmed_password, :string
     add_column :users, :introduction, :text
     add_column :users, :image, :string
